66167cf8d18f33b5697aa3a1437a30b98865568a
Alex-HP <aleksandr.lozinskiy2005@gmail.com>
2024-02-21 19:26:34 -0700

    Notes app task 1 is done

---
+++ notes.controller.js
@@ -0,0 +1,52 @@
+const fs = require("fs/promises");
+const path = require("path");
+const chalk = require("chalk");
+const _ = require("lodash");
+
+const notesPath = path.join(__dirname, "db.json");
+
+async function addNote(title) {
+    const notes = await getNotes();
+
+    const note = {
+        id: Date.now().toString(),
+        title,
+    };
+
+    notes.push(note);
+
+    await fs.writeFile(notesPath, JSON.stringify(notes));
+    console.log(chalk.green.inverse("Note was added"));
+}
+
+async function removeNote(id) {
+    const oldNotes = await getNotes();
+    const newNotes = oldNotes.filter((note) => note.id !== id);
+
+    if (!_.isEqual(oldNotes, newNotes)) {
+        await fs.writeFile(notesPath, JSON.stringify(newNotes));
+        console.log(chalk.green.inverse(`Note with id ${id} was deleted`));
+    } else {
+        console.log(chalk.red.inverse(`Note with id ${id} wasn't found`));
+    }
+}
+
+async function printNotes() {
+    const notes = await getNotes();
+
+    console.log(chalk.bgBlueBright("Here is the list of notes"));
+    notes.forEach((note) => {
+        console.log(chalk.blueBright(`id: ${note.id}  title:${note.title}`));
+    });
+}
+
+async function getNotes() {
+    const notes = await fs.readFile(notesPath, { encoding: "utf-8" });
+    return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : [];
+}
+
+module.exports = {
+    addNote,
+    removeNote,
+    printNotes,
+};

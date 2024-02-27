14b4d50b68f7f385b5a989d25cdc9f99a1859051
Alex-HP <aleksandr.lozinskiy2005@gmail.com>
2024-02-27 16:51:26 -0700

    additional task2 is done

--- public/app.js
+++ public/app.js
@@ -1,3 +1,8 @@
+const itemDisplayFlex =
+    "item-wrapper d-flex justify-content-between align-items-center";
+const itemDisplayNone =
+    "item-wrapper d-none justify-content-between align-items-center";
+
 document.addEventListener("click", ({ target }) => {
     if (target.dataset.type === "remove") {
         const id = target.dataset.id;
@@ -5,18 +10,45 @@ document.addEventListener("click", ({ target }) => {
         remove(id).then(() => {
             target.closest("li").remove();
         });
-    } else if (target.dataset.type === "edit") {
+    } else if (
+        target.dataset.type === "edit" ||
+        target.dataset.type === "cancel"
+    ) {
+        toggleItemEditWrappers(target);
+    } else if (target.dataset.type === "save") {
         const id = target.dataset.id;
-        const newTitle = prompt("Введите новое название: ");
+        const input = target.closest(".item-wrapper").querySelector("input");
+
+        const title = target
+            .closest("li")
+            .querySelector(".item-wrapper.d-none > .title");
+
+        const newTitle = input.value;
 
         if (newTitle) {
             edit(id, newTitle).then(() => {
-                target.closest("li").querySelector(".title").textContent =
-                    newTitle;
+                toggleItemEditWrappers(target);
+                title.innerHTML = newTitle;
             });
         }
     }
 });
+
+const noteAddMessage = document.querySelector(".alert");
+if (noteAddMessage) {
+    setTimeout(() => {
+        noteAddMessage.remove();
+    }, 1500);
+}
+
+function toggleItemEditWrappers(target) {
+    const itemWrapper = target.closest(".item-wrapper.d-flex");
+    const itemWrapperEdit = target
+        .closest("li")
+        .querySelector(".item-wrapper.d-none");
+    itemWrapper.className = itemDisplayNone;
+    itemWrapperEdit.className = itemDisplayFlex;
+}
 
 async function edit(id, title) {
     const data = { id, title };
